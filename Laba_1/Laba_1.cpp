e0a75561b827c03649cc7bfce9603ad492e21806
Stanislav <Stanislav@yandex.ru>
2024-09-05 22:44:55 +0300

    Part1

---
+++ Laba_1/Laba_1.cpp
@@ -0,0 +1,35 @@
+﻿// Laba_1.cpp 
+// part_1
+
+
+#include <iostream>
+
+int main()
+{
+	int min = INT_MAX, max = INT_MIN;
+	int a[10] = { 0 };
+
+	for (int i = 0; i < 10; i++) {
+		a[i] = i;
+		printf("%d ", a[i]);
+	}
+
+	for (int i = 0; i < 10; i++) {
+		if (a[i] < min) {
+			min = a[i]; 
+		};
+	}
+
+	for (int i = 0; i < 10; i++) {
+		if (a[i] < min) {
+			min = a[i]; 
+		};
+	}
+
+	printf("\n%d", max - min);
+	printf("\n%d", max);
+	printf("\n%d", min);
+
+
+	return 0;
+}
