808e7ec1410b280f535b7a749e576daeb4d115f4
Stanislav <Stanislav@yandex.ru>
2024-09-06 02:38:49 +0300

    Part4

--- Laba_1/Laba_1.cpp
+++ Laba_1/Laba_1.cpp
@@ -1,28 +1,56 @@
 ﻿// Laba_1.cpp 
-// part_3
+// part_4
 
-
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <malloc.h>
+
 
 int main()
 {
-	int* a;
-	int n, z = 0;
-	printf("Input massive size: ");
-	scanf_s("%d", &n);
+    int* a; 
+    int i, j, n, m;
+    int sum=0 ;
+ 
+    printf("Input row: ");
+    scanf("%d", &n);
+    printf("Input column: ");
+    scanf("%d", &m);
 
-	a = (int*)malloc(n * sizeof(int));
-	for (int i = 0; i < n; i++) {
-		printf("Input element num %d : ", i + 1);
-		scanf_s("%d", &z);
-		a[i] = z;
-	}
 
-	printf("\n");
+    a = (int*)malloc(n * m * sizeof(int));
 
-	for (int i = 0; i < n; i++) {
-		printf("%d ", a[i]);
-	}
+    for (i = 0; i < n; i++) 
+    {
+        for (j = 0; j < m; j++)
+        {
+            printf("a[%d][%d] = ", i, j);
+            scanf("%d", (a + i * m + j));
+        }
+    }
+   
 
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < m; j++)
+        {
+            sum = sum + *(a + i * m + j);
+        }
+        printf("Sum in the row num %d = %d\n", i+1, sum);
+        sum = 0;
+    }
+
+
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < m; j++)
+        {
+            printf("%4d ", *(a + i * m + j));
+
+            
+        }
+        printf("\n");
+    }
+        free(a);
+        getchar();
 	return 0;
 }
